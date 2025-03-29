e296b1a4741ad4366fec7efc5f6937e89c99e183
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-03-29 15:25:42 -0400

    Initial commit

---
+++ be-joining.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
+import { BE } from 'be-enhanced/BE.js';
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-joining/types' */;
+/** @import {EnhancementInfo} from './ts-refs/trans-render/be/types' */
+
+
+/**
+ * @implements {Actions}
+ * 
+ */
+class BeJoining extends BE {
+    /**
+     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement>}
+     */
+    static config = {
+    };
+}
+
+await BeJoining.bootUp();
+export {BeJoining};
